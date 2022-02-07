d9bff7ca97e5af44df3be1c136929061ffa508e9
WiwilZ <56433705+WiwilZ@users.noreply.github.com>
2022-02-07 20:16:24 +0800

    Update random.h

--- RealRandom/random.h
+++ RealRandom/random.h
@@ -11,7 +11,7 @@
 #include <unistd.h>
 
 
-void __Random(void* buffer, size_t size) {
+void random_buffer(void* buffer, size_t size) {
     if (int fd = open("/dev/urandom", O_RDONLY); read(fd, buffer, size) == size) {
         close(fd);
         return;
@@ -19,33 +19,22 @@ void __Random(void* buffer, size_t size) {
     throw std::runtime_error("`/dev/urandom` read error");
 }
 
-
 template<typename T>
 T random_integer() requires(std::is_arithmetic_v<T>) {
     T res;
-    __Random(&res, sizeof(T));
+    random_buffer(&res, sizeof(T));
     return res;
 }
+
 
 template<typename T, typename Container = std::pmr::vector<T>>
 Container random_integer(size_t n) requires(std::is_arithmetic_v<T>) {
     constexpr size_t alignment = alignof(T);
     auto buffer = new(std::align_val_t{ alignment }) T[n];
-    __Random(buffer, n * sizeof(T));
+    random_buffer(buffer, n * sizeof(T));
     std::pmr::vector<T> res(buffer, buffer + n);
     ::operator delete[](buffer, std::align_val_t{ alignment });
     return res;
-}
-
-
-std::shared_ptr<std::byte> random_buffer(size_t size) {
-    if (size == 0) {
-        return nullptr;
-    }
-
-    auto buffer = new std::byte[size];
-    __Random(buffer, size);
-    return std::shared_ptr<std::byte>{ buffer };
 }
 
 
@@ -57,7 +46,7 @@ std::pmr::string random_string(size_t size) {
     constexpr size_t alignment = alignof(__m512i);
     size_t newSize = size <= alignment ? alignment : std::bit_ceil(size);
     auto buffer = new(std::align_val_t{ alignment }) char[newSize];
-    __Random(buffer, newSize);
+    random_buffer(buffer, newSize);
     const __m512i mask = _mm512_set1_epi8(0b01111111);
     for (auto p = buffer; p < buffer + newSize; p += alignment) {
         _mm512_store_si512(p, _mm512_and_si512(_mm512_load_si512(p), mask));
