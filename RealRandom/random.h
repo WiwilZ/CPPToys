718efd0515f98c64e5b296ed5b1a3830e1bb9c26
WiwilZ <56433705+WiwilZ@users.noreply.github.com>
2022-02-07 19:54:53 +0800

    Update random.h

--- RealRandom/random.h
+++ RealRandom/random.h
@@ -1,8 +1,10 @@
 #pragma once
 
+
 #include <bit>
 #include <stdexcept>
 #include <concepts>
+#include <memory_resource>
 #include <memory>
 #include <x86intrin.h>
 #include <fcntl.h>
@@ -25,6 +27,16 @@ T random_integer() requires(std::is_arithmetic_v<T>) {
     return res;
 }
 
+template<typename T, typename Container = std::pmr::vector<T>>
+Container random_integer(size_t n) requires(std::is_arithmetic_v<T>) {
+    constexpr size_t alignment = alignof(T);
+    auto buffer = new(std::align_val_t{ alignment }) T[n];
+    __Random(buffer, n * sizeof(T));
+    std::pmr::vector<T> res(buffer, buffer + n);
+    ::operator delete[](buffer, std::align_val_t{ alignment });
+    return res;
+}
+
 
 std::shared_ptr<std::byte> random_buffer(size_t size) {
     if (size == 0) {
@@ -37,20 +49,20 @@ std::shared_ptr<std::byte> random_buffer(size_t size) {
 }
 
 
-std::string random_string(size_t size) {
+std::pmr::string random_string(size_t size) {
     if (size == 0) {
-        return std::string{};
+        return {};
     }
 
     constexpr size_t alignment = alignof(__m512i);
-    size_t alignedSize = size <= alignment ? alignment : std::bit_ceil(size);
-    auto buffer = new(std::align_val_t{ alignment }) char[alignedSize];
-    __Random(buffer, alignedSize);
+    size_t newSize = size <= alignment ? alignment : std::bit_ceil(size);
+    auto buffer = new(std::align_val_t{ alignment }) char[newSize];
+    __Random(buffer, newSize);
     const __m512i mask = _mm512_set1_epi8(0b01111111);
-    for (auto p = buffer; p < buffer + alignedSize; p += alignment) {
+    for (auto p = buffer; p < buffer + newSize; p += alignment) {
         _mm512_store_si512(p, _mm512_and_si512(_mm512_load_si512(p), mask));
     }
-    std::string res(buffer, buffer + size);
+    std::pmr::string res(buffer, buffer + size);
     ::operator delete[](buffer, std::align_val_t{ alignment });
     return res;
 }
